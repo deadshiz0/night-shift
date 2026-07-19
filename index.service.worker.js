a72cf02c3473c65c52b6bd9e86965a68dff58cd7
deadshiz0 <deadshiz0@gmx.net>
2026-07-19 20:32:40 +0200

    Fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784458641|65735295';
+const CACHE_VERSION = '1784485938|13730964122';
 /** @type {string} */
 const CACHE_PREFIX = 'NIGHT SHIFT-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
